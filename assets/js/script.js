a02e62ac44fa084c2f975cb64272cf60e35e6e67
urlDiego <diegoarmandogomezvasquez2003@gmail.com>
2024-07-02 16:51:48 -0600

    v2.0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,39 @@
-const passwordBox = document.getElementById("password")
-const lenght = 12
+const passwordBox = document.getElementById("password");
+const lenghtInput = document.getElementById("input-lenght");
 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const lowerCase = "abcdefghijklmnopqrstuvwxyz"
-const number = "0123456789"
-const symbol = "@#$%^&*()_+~|}{][></-="
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{][></-=";
 
-const allChars = upperCase + lowerCase + number + symbol
+const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword() {
-    let password = ""
-    password += upperCase[Math.floor(Math.random() * upperCase.length)]
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
-    password += number[Math.floor(Math.random() * number.length)]
-    password += symbol[Math.floor(Math.random() * symbol.length)]
+  if (lenghtInput.value == "") {
+    alert("Debes escribir la longitud!");
+    return;
+  } else if (lenghtInput.value < 4) {
+    alert("Debe ser mayor a 4");
+    return;
+  } else if (lenghtInput.value > 15) {
+    alert("Debe ser menor a 15!");
+    return;
+  } else {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
 
-    while (lenght > password.length) {
-        password += allChars[Math.floor(Math.random() * allChars.length)]
+    while (lenghtInput.value > password.length) {
+      password += allChars[Math.floor(Math.random() * allChars.length)];
     }
 
-    passwordBox.value = password
+    passwordBox.value = password;
+  }
 }
 
 function copyPassword() {
-    passwordBox.select()
-    document.execCommand("copy")
+  passwordBox.select();
+  document.execCommand("copy");
 }
